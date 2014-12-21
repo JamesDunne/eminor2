ef80b3392e3d00be4f7c276e25e6c42d7091cf5c
James Dunne <james.jdunne@gmail.com>
2014-12-21 08:03:42 +0000

    Clearing out more unused code to free up space for song data.

--- v1/controller/PIC/startup.c
+++ v1/controller/PIC/startup.c
@@ -9,15 +9,14 @@ extern void main (void);
 /* prototype for the startup function */
 void _entry (void);
 void _startup (void);
-extern near char __FPFLAGS;
+//extern near char __FPFLAGS;
 #define RND 6
 
 #pragma code _entry_scn=0x000A00
 void
 _entry (void)
 {
-_asm goto _startup _endasm
-
+  _asm goto _startup _endasm
 }
 
 #pragma code _startup_scn
@@ -31,7 +30,7 @@ _startup (void)
 
     clrf TBLPTRU, 0 // 1st silicon doesn't do this on POR
 
-    bcf __FPFLAGS,RND,0 // Initalize rounding flag for floating point libs
+    //bcf __FPFLAGS,RND,0 // Initalize rounding flag for floating point libs
 
 	clrf	FSR0H, 0	//the 0 indicates its in access ram
 	clrf	FSR0L, 0
