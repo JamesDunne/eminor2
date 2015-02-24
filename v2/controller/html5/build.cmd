abb64050b8962e9321f73463499abe62792fb2b6
James Dunne <james.jdunne@gmail.com>
2015-02-24 00:07:29 +0000

    Upgrading emscripten build system to latest emscripten and upgraded html5 driver per recent hardware.h changes.

--- v2/controller/html5/build.cmd
+++ v2/controller/html5/build.cmd
@@ -1,8 +1,13 @@
 @rem Removed '$Browser' from default DEFAULT_LIBRARY_FUNCS_TO_INCLUDE in src/settings.js to reduce size by ~100KB.
 @echo Building eminorv2.js
-call emcc hardware.c ../common/controller-simple.c -o www/src/eminorv2.js -s TOTAL_STACK="192" -s TOTAL_MEMORY="2048" -s FAST_MEMORY="2048" -s EXPORT_NAME="'eminorv2'" -s EXPORTED_FUNCTIONS="['_controller_init','_controller_10msec_timer','_controller_handle']" -s DEFAULT_LIBRARY_FUNCS_TO_INCLUDE="['memcpy','memset','malloc','free','strlen']" -s LINKABLE="1" -O2 --closure 1
+call emcc hardware.c ../common/controller-simple.c -o www/src/eminorv2.js -s TOTAL_STACK="192" -s TOTAL_MEMORY="2048" -s FAST_MEMORY="2048" -s EXPORT_NAME="'eminorv2'" -s EXPORTED_FUNCTIONS="['_controller_init','_controller_10msec_timer','_controller_handle']" -s DEFAULT_LIBRARY_FUNCS_TO_INCLUDE="['memcpy','memset','malloc','free','strlen','$Browser']" -s LINKABLE="1" -s ASSERTIONS="1" --closure 1
+
+rem NOTE(jsd): Removing -O2 fixes future compilation problems with compiler.jar
+REM --closure 1 -O2
+
 @echo Compiling all javascript into all.min.js
 pushd www\src
 java -jar ..\..\compiler.jar --js es5.js web.js typedarray.js storage.js console.js rainbow.js c.js generic.js eminorv2.js driver.js --js_output_file ..\all.min.js
+move /Y eminorv2.js.mem ..
 popd
 @echo Complete!
