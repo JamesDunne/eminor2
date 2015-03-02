83aa309b673b7b274cb27c5595d6a4f86d79a8e3
James Dunne <jdunne@kaplan.edu>
2015-03-01 21:41:02 -0600

    Removed flash.hex dependency and HTML5 flash interface in favor of hard-coded flash ROM data.

--- v2/controller/html5/hardware.c
+++ v2/controller/html5/hardware.c
@@ -1,2 +1,21 @@
 #include "../common/types.h"
 #include "../common/hardware.h"
+
+#define FLASH_LENGTH (sizeof(struct program) * 128 + sizeof(struct set_list) * 32)
+const int flash_length = FLASH_LENGTH;
+const unsigned char flash_memory[FLASH_LENGTH] = {
+#include "../PIC/flash_rom_init.h"
+};
+#undef FLASH_LENGTH
+
+// Load `count` bytes from flash memory at address `addr` into `data`:
+void flash_load(u16 addr, u16 count, u8 *data)
+{
+	memcpy((void *)data, (void *)&flash_memory[addr], count);
+}
+
+// Stores `count` bytes from `data` into flash memory at address `addr`:
+void flash_store(u16 addr, u16 count, u8 *data)
+{
+	memcpy((void *)&flash_memory[addr], (void *)data, count);
+}
