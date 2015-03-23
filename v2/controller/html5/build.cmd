1e667552d857fe7f97fb18507f8afc05e3679d96
James Dunne <james.jdunne@gmail.com>
2015-03-23 01:22:14 -0500

    Rebuilt for html5.

--- v2/controller/html5/build.cmd
+++ v2/controller/html5/build.cmd
@@ -1,13 +1,8 @@
 @rem Removed '$Browser' from default DEFAULT_LIBRARY_FUNCS_TO_INCLUDE in src/settings.js to reduce size by ~100KB.
 @echo Building eminorv2.js
-call emcc hardware.c ../common/controller-simple.c -o www/src/eminorv2.js -s TOTAL_STACK="192" -s TOTAL_MEMORY="2048" -s FAST_MEMORY="2048" -s EXPORT_NAME="'eminorv2'" -s EXPORTED_FUNCTIONS="['_controller_init','_controller_10msec_timer','_controller_handle']" -s DEFAULT_LIBRARY_FUNCS_TO_INCLUDE="['memcpy','memset','malloc','free','strlen','$Browser']" -s LINKABLE="1" -s ASSERTIONS="1" --closure 1
-
-rem NOTE(jsd): Removing -O2 fixes future compilation problems with compiler.jar
-REM --closure 1 -O2
+call emcc hardware.c ../common/controller-simple.c -o eminorv2.gen.js -s TOTAL_STACK="192" -s TOTAL_MEMORY="2048" -s FAST_MEMORY="2048" -s EXPORT_NAME="'eminorv2'" -s EXPORTED_FUNCTIONS="['_controller_init','_controller_10msec_timer','_controller_handle']" -s DEFAULT_LIBRARY_FUNCS_TO_INCLUDE="['memcpy','memset','malloc','free','strlen','$Browser']" -s LINKABLE="1"
 
 @echo Compiling all javascript into all.min.js
-pushd www\src
-java -jar ..\..\compiler.jar --js es5.js web.js typedarray.js storage.js console.js rainbow.js c.js generic.js eminorv2.js driver.js --js_output_file ..\all.min.js
-move /Y eminorv2.js.mem ..
-popd
+java -jar compiler.jar --jscomp_off suspiciousCode --jscomp_off uselessCode --language_in ECMASCRIPT5 --js es5.js web.js typedarray.js storage.js console.js rainbow.js c.js generic.js eminorv2.gen.js driver.js --js_output_file www\all.min.js
+
 @echo Complete!
