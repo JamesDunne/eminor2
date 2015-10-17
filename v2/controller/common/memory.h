ab4a39b9c56023b6883033fdcd0bd022f7903487
James Dunne <james.jdunne@gmail.com>
2015-10-16 22:59:40 -0500

    Corrected documentation.

--- v2/controller/common/memory.h
+++ v2/controller/common/memory.h
@@ -12,6 +12,27 @@ struct program {
     u8 fx[6];
 };
 
+// Mark V channel 1
+#define rjm_channel_1   0x00
+// Mark V channel 2
+#define rjm_channel_2   0x01
+// Mark V channel 3
+#define rjm_channel_3   0x02
+
+#define rjm_channel_mask        0x03
+
+// Scene level as 2-bit signed integer
+#define scene_level_mask (31 << 2)
+#define scene_level_shr  2
+
+// 5-bit signed values
+#define scene_level_offset  9
+#define scene_level_0    ((( 0 + scene_level_offset) & 31) << 2)
+#define scene_level_pos4 (((+5 + scene_level_offset) & 31) << 2)
+#define scene_level_neg3 (((-3 + scene_level_offset) & 31) << 2)
+
+#define scene_initial    0x80
+
 // NOTE(jsd): Struct size must be a divisor of 64 to avoid crossing 64-byte boundaries in flash!
 // Struct sizes of 1, 2, 4, 8, 16, and 32 qualify.
 COMPILE_ASSERT(sizeof(struct program) == 32);
@@ -38,42 +59,3 @@ struct set_list {
 // NOTE(jsd): Struct size must be a divisor of 64 to avoid crossing 64-byte boundaries in flash!
 // Struct sizes of 1, 2, 4, 8, 16, and 32 qualify.
 COMPILE_ASSERT(sizeof(struct set_list) == 64);
-
-// An RJM channel descriptor:
-//
-// 8 bits describes two RJM channels:
-//
-// bits:
-// 7654 3210
-// BBCC BBCC
-// |||| ||||
-// |||| ||\+- Channel (0-2, 3 ignored)
-// |||| ||
-// |||| \+--- Boost   -6dB, -3dB, 0dB, +3dB (signed 2-bit)
-// ||||
-// ||\+------ Channel (0-2, 3 ignored)
-// ||
-// \+-------- Boost   -6dB, -3dB, 0dB, +3dB (signed 2-bit)
-//
-
-
-// Mark V channel 1
-#define rjm_channel_1   0x00
-// Mark V channel 2
-#define rjm_channel_2   0x01
-// Mark V channel 3
-#define rjm_channel_3   0x02
-
-#define rjm_channel_mask        0x03
-
-// Scene level as 2-bit signed integer
-#define scene_level_mask (31 << 2)
-#define scene_level_shr  2
-
-// 5-bit signed values
-#define scene_level_offset  9
-#define scene_level_0    ((( 0 + scene_level_offset) & 31) << 2)
-#define scene_level_pos4 (((+5 + scene_level_offset) & 31) << 2)
-#define scene_level_neg3 (((-3 + scene_level_offset) & 31) << 2)
-
-#define scene_initial    0x80
