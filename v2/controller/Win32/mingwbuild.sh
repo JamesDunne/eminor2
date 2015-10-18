7e67a72762b1d2e0d8837451ff470caae349e216
James Dunne <james.jdunne@gmail.com>
2015-10-18 12:56:41 -0500

    Split out source files for mingwbuild.sh and corrected source filename.

--- v2/controller/Win32/mingwbuild.sh
+++ v2/controller/Win32/mingwbuild.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
-gcc -std=c99 -D UNICODE -D _UNICODE -D_WIN32_WINNT=0x0601 eminorv2.c ../common/controller-simple.c -Wl,--subsystem,windows -lkernel32 -luser32 -lgdi32 -lwinmm -o eminorv2.exe
+#!/bin/bash
+SOURCES="eminorv2.c ../common/controller.c"
+gcc -std=c99 -D UNICODE -D _UNICODE -D_WIN32_WINNT=0x0601 $SOURCES -Wl,--subsystem,windows -lkernel32 -luser32 -lgdi32 -lwinmm -o eminorv2.exe
