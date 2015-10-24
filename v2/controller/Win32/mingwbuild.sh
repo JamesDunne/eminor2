fb4586d23d986c423f3774bc11f1c05f9ad034cc
James Dunne <james.jdunne@gmail.com>
2015-10-24 16:09:01 -0500

    Compiling controller-one.c

--- v2/controller/Win32/mingwbuild.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-SOURCES="eminorv2.c ../common/controller.c"
-gcc -std=c99 -D HWFEAT_LABEL_UPDATES -D UNICODE -D _UNICODE -D_WIN32_WINNT=0x0601 $SOURCES -Wl,--subsystem,windows -lkernel32 -luser32 -lgdi32 -lwinmm -o eminorv2.exe
