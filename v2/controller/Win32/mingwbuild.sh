ace5577f77c3eed93f0eb33f30ab04dd3443b347
James Dunne <james.jdunne@gmail.com>
2015-10-27 20:16:17 -0500

    Updated Win32 build.

---
+++ v2/controller/Win32/mingwbuild.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -std=c99 -DHW_VERSION=1 -DHWFEAT_LABEL_UPDATES -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0601 eminorv2.c ../common/controller-one.c ../common/controller-two.c -Wl,--subsystem,windows -lkernel32 -luser32 -lgdi32 -lwinmm -o eminorv2.exe
