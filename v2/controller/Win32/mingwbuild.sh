bed8a1865523a11ff3a871ae39b68419aa62c064
James Dunne <james.jdunne@gmail.com>
2016-02-12 18:12:39 -0600

    Rebuild PIC.

--- v2/controller/Win32/mingwbuild.sh
+++ v2/controller/Win32/mingwbuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-HW_VERSION=1
+HW_VERSION=2
 gcc -std=c99 -DHW_VERSION=$HW_VERSION -DHWFEAT_LABEL_UPDATES -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0601 eminorv2.c ../common/controller-one.c ../common/controller-two.c -Wl,--subsystem,windows -lkernel32 -luser32 -lgdi32 -lwinmm -o eminorv2.exe
