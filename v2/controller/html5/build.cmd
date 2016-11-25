da9c19fa6b5fb1a74088e0cdb5e7d047cce1cf20
James Dunne <james.jdunne@gmail.com>
2016-11-25 12:03:44 -0600

    Recompiled html5 with latest emscripten version 1.35.0.

--- v2/controller/html5/build.cmd
+++ v2/controller/html5/build.cmd
@@ -1,8 +1,10 @@
-@rem Removed '$Browser' from default DEFAULT_LIBRARY_FUNCS_TO_INCLUDE in src/settings.js to reduce size by ~100KB.
-@echo Building eminorv2.js
-call emsdk hardware.c ../common/controller-simple.c -o eminorv2.gen.js -s TOTAL_STACK="192" -s TOTAL_MEMORY="2048" -s FAST_MEMORY="2048" -s EXPORT_NAME="'eminorv2'" -s EXPORTED_FUNCTIONS="['_controller_init','_controller_10msec_timer','_controller_handle']" -s DEFAULT_LIBRARY_FUNCS_TO_INCLUDE="['memcpy','memset','malloc','free','strlen','$Browser']" -s LINKABLE="1"
+@echo off
 
-@echo Compiling all javascript into all.min.js
+rem Removed '$Browser' from default DEFAULT_LIBRARY_FUNCS_TO_INCLUDE in src/settings.js to reduce size by ~100KB.
+echo Building eminorv2.js
+call emcc -DHW_VERSION=4 hardware.c ../common/controller-axe.c -o eminorv2.gen.js -s EXPORT_NAME="'eminorv2'" -s EXPORTED_FUNCTIONS="['_controller_init','_controller_10msec_timer','_controller_handle']" -s DEFAULT_LIBRARY_FUNCS_TO_INCLUDE="['memcpy','memset','malloc','free','strlen','$Browser']" -s LINKABLE="1"
+
+echo Compiling all javascript into all.min.js
 java -jar compiler.jar --jscomp_off suspiciousCode --jscomp_off uselessCode --language_in ECMASCRIPT5 --js es5.js web.js typedarray.js storage.js console.js rainbow.js c.js generic.js eminorv2.gen.js driver.js --js_output_file www\all.min.js
 
-@echo Complete!
+echo Complete!
--- v2/controller/html5/hardware.c
+++ v2/controller/html5/hardware.c
@@ -1,4 +1,6 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 #include "../common/types.h"
 #include "../common/hardware.h"
@@ -6,11 +8,17 @@
 #define FLASH_LENGTH (64 * 128 + 64 * 32)
 const int flash_length = FLASH_LENGTH;
 const unsigned char flash_memory[FLASH_LENGTH] = {
-#include "../PIC/flash_v1_bank0.h"
+#include "../PIC/flash_v4_bank0.h"
 ,
-#include "../PIC/flash_v1_bank1.h"
+#include "../PIC/flash_v4_bank1.h"
+,
+#include "../PIC/flash_v4_bank2.h"
 };
 #undef FLASH_LENGTH
+
+const unsigned char lookup[128] = {
+#include "../PIC/v4_lookup.h"
+};
 
 // Load `count` bytes from flash memory at address `addr` into `data`:
 void flash_load(u16 addr, u16 count, u8 *data)
@@ -22,4 +30,22 @@ void flash_load(u16 addr, u16 count, u8 *data)
 void flash_store(u16 addr, u16 count, u8 *data)
 {
 	memcpy((void *)&flash_memory[addr], (void *)data, count);
+}
+
+rom const u8 *flash_addr(u16 addr) {
+	return &flash_memory[addr];
+}
+
+// Get a pointer to a lookup table:
+rom const u8 *lookup_table(u8 table) {
+	return lookup;
+}
+
+void debug_log(const char *fmt, ...) {
+    va_list ap;
+    printf("DEBUG: ");
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+    printf("\n");
 }
