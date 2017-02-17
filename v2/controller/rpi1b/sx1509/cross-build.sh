4174b3eb315dd93b665f71b1ff8d0c9cd6c54628
James Dunne <james.jdunne@gmail.com>
2017-02-16 22:55:18 -0600

    Thinking correctly about Linux I2C clients now. Starting to write SX1509 interface functions.

---
+++ v2/controller/rpi1b/sx1509/cross-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# http://www.welzels.de/blog/en/arm-cross-compiling-with-mac-os-x/
+CC=/usr/local/linaro/arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc
+
+SRCS=(*.c)
+
+$CC -g -DHW_VERSION=4 -I../common "${SRCS[@]}"
