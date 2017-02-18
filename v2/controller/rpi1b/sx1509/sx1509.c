782718fbd19d5232ac5d5fd27b4b89025063f14d
James Dunne <james.jdunne@gmail.com>
2017-02-18 12:24:25 -0600

    Invert button state due to active low. Better output.

--- v2/controller/rpi1b/sx1509/sx1509.c
+++ v2/controller/rpi1b/sx1509/sx1509.c
@@ -17,7 +17,7 @@ u16 sx1509_read_data(u8 slave_addr) {
     if (i2c_read(slave_addr, REG_DATA_B, &buf[1]) != 0) {
         return 0;
     }
-    return ((u16)buf[1] << 8) | (u16)buf[0];
+    return ~(u16)( ((u16)buf[1] << 8) | (u16)buf[0] );
 }
 
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
@@ -41,20 +41,19 @@ int main() {
     if (sx1509_set_register(slave_addr, REG_INPUT_DISABLE_B, 0x00) != 0) goto fail;
 
     // Pull-up resistor on button pins:
-    if (sx1509_set_register(slave_addr, REG_PULL_UP_A, 0xFF) != 0) goto fail;
-    if (sx1509_set_register(slave_addr, REG_PULL_UP_B, 0xFF) != 0) goto fail;
+    if (sx1509_set_register(slave_addr, REG_PULL_UP_A,   0xFF) != 0) goto fail;
+    if (sx1509_set_register(slave_addr, REG_PULL_UP_B,   0xFF) != 0) goto fail;
     if (sx1509_set_register(slave_addr, REG_PULL_DOWN_A, 0x00) != 0) goto fail;
     if (sx1509_set_register(slave_addr, REG_PULL_DOWN_B, 0x00) != 0) goto fail;
 
     while (1) {
         u16 buttons = sx1509_read_data(slave_addr);
         printf(
-            "%04X "BYTE_TO_BINARY_PATTERN""BYTE_TO_BINARY_PATTERN"\n",
-            buttons,
+            ""BYTE_TO_BINARY_PATTERN" "BYTE_TO_BINARY_PATTERN"\n",
             BYTE_TO_BINARY(buttons>>8),
             BYTE_TO_BINARY(buttons)
         );
-        usleep(50L * 1000L);
+        usleep(10L * 1000L);
     }
 
 fail:
