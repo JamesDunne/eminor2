c18b50859247e88d6436096691d6877ba508dc83
James Dunne <james.jdunne@gmail.com>
2017-03-01 19:27:11 -0600

    Added REG_DIR initialization to set input/output direction on pins.

--- v2/controller/rpi1b/fsw.c
+++ v2/controller/rpi1b/fsw.c
@@ -8,11 +8,13 @@ typedef u8 byte;
 
 // Initialize SX1509 for buttons by enabling all pins as inputs and pull-up resistors:
 int fsw_init(void) {
-    // Enable all inputs:
+    // Set all pins as inputs:
+    if (i2c_write(i2c_sx1509_btn_addr, REG_DIR_A,           0xFF) != 0) goto fail;
+    if (i2c_write(i2c_sx1509_btn_addr, REG_DIR_B,           0xFF) != 0) goto fail;
     if (i2c_write(i2c_sx1509_btn_addr, REG_INPUT_DISABLE_A, 0x00) != 0) goto fail;
     if (i2c_write(i2c_sx1509_btn_addr, REG_INPUT_DISABLE_B, 0x00) != 0) goto fail;
 
-    // Pull-up resistor on button pins:
+    // Pull-up resistor on all pins for active-low buttons:
     if (i2c_write(i2c_sx1509_btn_addr, REG_PULL_UP_A,   0xFF) != 0) goto fail;
     if (i2c_write(i2c_sx1509_btn_addr, REG_PULL_UP_B,   0xFF) != 0) goto fail;
     if (i2c_write(i2c_sx1509_btn_addr, REG_PULL_DOWN_A, 0x00) != 0) goto fail;
@@ -27,7 +29,9 @@ fail:
 // Poll 16 foot-switch states:
 u16 fsw_poll(void) {
     u8 buf[2];
+    // Read both data registers to get entire 16 bits of input state:
     if (i2c_read(i2c_sx1509_btn_addr, REG_DATA_A, &buf[0]) != 0) return 0;
     if (i2c_read(i2c_sx1509_btn_addr, REG_DATA_B, &buf[1]) != 0) return 0;
+    // NOTE: we invert (~) button state because they are active low (0) and default high (1):
     return ~(u16)( ((u16)buf[1] << 8) | (u16)buf[0] );
 }
