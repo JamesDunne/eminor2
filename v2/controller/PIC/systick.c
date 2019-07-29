5ef75f4b18081ece12cd2f48e21c2616e8b7be0b
James Dunne <james.jdunne@gmail.com>
2019-07-29 12:15:35 -0500

    Corrected timer to use 1ms ticks instead of 10ms ticks.

--- v2/controller/PIC/systick.c
+++ v2/controller/PIC/systick.c
@@ -19,7 +19,15 @@ void systick_init(void) {
 }
 
 //1mS time keeping routine:
-void    SystemTimeRoutine(void) {
+void SystemTimeRoutine(void) {
+    // Timers:
+    if (timer[0] != 0xFFFF) {
+        timer[0]++;
+    }
+    if (timer[1] != 0xFFFF) {
+        timer[1]++;
+    }
+
     SystickCntr2++;
     if (SystickCntr2 == SYSTEM_TIME_10MS) {
         SystickCntr2 = 0;
@@ -28,14 +36,6 @@ void    SystemTimeRoutine(void) {
         HandleController = true;
         HandleLeds = true;
         ControllerTiming = true;
-
-        // Timers:
-        if (timer[0] != 0xFFFF) {
-            timer[0]++;
-        }
-        if (timer[1] != 0xFFFF) {
-            timer[1]++;
-        }
 
         SystickCntr3++;
         if (SystickCntr3 == SYSTEM_TIME_40MS) {
