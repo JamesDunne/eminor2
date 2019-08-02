bc2b128ebdc006393104e65dff2375cd096eeb24
James Dunne <james.jdunne@gmail.com>
2019-08-01 21:23:33 -0500

    Rebuilt HEX; reduced flash size to accomodate code

--- v2/controller/common/flash.h
+++ v2/controller/common/flash.h
@@ -1,4 +1,4 @@
 
 //Size of writable flash segment:
-#define WRITABLE_SEG_ADDR       0x5300      //Also update this in the lkr file if it needs to change!!
+#define WRITABLE_SEG_ADDR       0x5400      //Also update this in the lkr file if it needs to change!!
 #define WRITABLE_SEG_LEN        0x8000-WRITABLE_SEG_ADDR
